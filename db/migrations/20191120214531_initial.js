6b6fe76c5718ca1166b041f1e147f77489adc9bd
Eduardo <jeduardorjx@gmail.com>
2019-11-21 11:36:19 -0700

    Setup schema tables

--- db/migrations/20191120214531_initial.js
+++ db/migrations/20191120214531_initial.js
@@ -1,8 +1,29 @@
 
 exports.up = function(knex) {
-  
+  return Promise.all([
+    knex.schema.createTable('houses', table => {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('mascot');
+      table.string('headOfHouse');
+      table.string('founder');
+
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('characters', table => {
+      table.increments('id').primary();
+      table.string('name');
+      table.integer('house_id').unsigned();
+      table.foreign('house_id').references('houses.id');
+
+      table.timestamps(true, true);
+
+    })
+  ]);
 };
 
 exports.down = function(knex) {
-  
+  knex.schema.dropTable('houses'),
+  knex.schema.dropTable('characters')
 };
