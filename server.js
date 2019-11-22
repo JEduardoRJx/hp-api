280943c190df148f9c355ac3ee058ba621c7ac71
Eduardo <jeduardorjx@gmail.com>
2019-11-22 10:50:48 -0700

    Change port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.emv.PORT || 3000;
+const port = process.env.PORT || 3000;
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
